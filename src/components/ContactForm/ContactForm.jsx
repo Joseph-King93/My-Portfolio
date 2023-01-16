f922c8d6684f8e67a0bd376929ccdab56383dbca
Joseph-King93 <j.king61693@gmail.com>
2023-01-15 19:47:44 -0500

    set up hooks for email form submit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -5,8 +5,12 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { useState } from 'react';
 
 export const ContactUs = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
     const form = useRef();
 
     const sendEmail = (e) => {
@@ -18,6 +22,9 @@ export const ContactUs = () => {
             }, (error) => {
                 console.log(error.text);
             });
+        setName('');
+        setEmail('');
+        setMessage('');
     };
 
     return (
@@ -28,13 +35,13 @@ export const ContactUs = () => {
                         <Col className="align-items-start">
                             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                                 <Form.Label style={{color: "#7f8ea8"}}><strong>Name</strong></Form.Label>
-                                <Form.Control type="text" placeholder="Your Name Here" name="user_name" />
+                                <Form.Control type="text" placeholder="Your Name Here" name="user_name" value={name} onChange={event => setName(event.target.value)}/>
                             </Form.Group>
                         </Col>
                         <Col>
                             <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
                                 <Form.Label style={{color: "#7f8ea8"}}><strong>Email address</strong></Form.Label>
-                                <Form.Control type="email" placeholder="YourEmail@Example.com" />
+                                <Form.Control type="email" placeholder="YourEmail@Example.com" value={email} onChange={event => setEmail(event.target.value)} />
                             </Form.Group>
                         </Col>
                     </Row>
@@ -42,7 +49,7 @@ export const ContactUs = () => {
                         <Col xs={12}>
                             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                                     <Form.Label style={{color: "#7f8ea8"}}><strong>Message Me!</strong></Form.Label>
-                                <Form.Control as="textarea" rows={3} name="message" placeholder='e.g. "We would like to Hire you!" or "Your account has been compromised. Kindly please clik this link to secure your account"'/>
+                                <Form.Control as="textarea" rows={3} name="message" placeholder='e.g. "We would like to Hire you!" or "Your account has been compromised. Kindly please clik this link to secure your account"' value={message} onChange={event => setMessage(event.target.value)} />
                             </Form.Group>
                         </Col>
                         <Col xs={{ span: 3, offset: 9 }} className='align-items-start'>
