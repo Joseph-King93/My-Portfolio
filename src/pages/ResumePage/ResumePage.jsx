5a2e910259c0997f70e6d0058b77e5e60cffbff2
Joseph-King93 <j.king61693@gmail.com>
2023-01-26 17:53:40 -0500

    update resumePage and PNG files

--- src/pages/ResumePage/ResumePage.jsx
+++ src/pages/ResumePage/ResumePage.jsx
@@ -7,7 +7,7 @@ import { Button } from 'react-bootstrap';
 export default function ResumePage() {
     return (
         <>
-            <Container className='text-center' style={{ margin: 0, padding: 25 }}>
+            <Container className='text-center' style={{ padding: 25 }}>
                 <h1 className='text-center' style={{ color: "#7f8ea8" }}>
                     SURPRISE!
                 </h1>
@@ -19,7 +19,7 @@ export default function ResumePage() {
                     Click <strong>The Button</strong> below!
                 </p>
                 <div className='text-center'>
-                    <a href="https://docs.google.com/document/d/1C0jQitn8GT9y1RPYY8Nd5bBMlQ_itzLf/edit?usp=sharing&ouid=114253510690230959510&rtpof=true&sd=true" target="_blank" rel="noreferrer">
+                    <a href="https://docs.google.com/document/d/1GAf4ut0Uqr0nrMjIupur-LwVj2o2npgi1kQTGYJb2H0/edit?usp=sharing" target="_blank" rel="noreferrer">
                         <Button><strong >The Button</strong></Button>
                     </a>
                 <p style={{fontSize: 12, marginTop: 20 }}>Too far...you passed the button</p>
@@ -29,29 +29,8 @@ export default function ResumePage() {
                             <Image
                                 rounded
                                 fluid
-                                src="/J-King-Resume-1.PNG"
+                                src="/Joseph-King-Resume.PNG"
                             />
-                    </Col>
-                </Row>
-                <br></br>
-                <Row>
-                    <Col>
-                        <Image
-                            rounded
-                            fluid
-                            src="/J-King-Resume-2.PNG"
-                        />
-                    </Col>
-
-                </Row>
-                <br></br>
-                <Row>
-                    <Col>
-                        <Image
-                            rounded
-                            fluid
-                            src="/J-King-Resume-3.PNG"
-                        />
                     </Col>
                 </Row>
             </Container>
